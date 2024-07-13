5c06baac10250b03382a0bd97bee883dfaa5bb24
geunhokinn <gghh1220@gmail.com>
2024-07-13 18:27:09 +0900

    test: MemberRepositoryTest 추가 - 페이징과 정렬이 적용된 조회 메서드를 테스트 하기 위해 searchPageComplexSort() 메서드 추가

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
@@ -217,5 +218,34 @@ class MemberRepositoryTest {
         for (Member findMember : result) {
             System.out.println("findMember = " + findMember);
         }
+    }
+
+    @Test
+    public void searchPageComplexSortTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Order.desc("username")));
+
+        Page<MemberTeamDto> result = memberRepository.searchComplexSort(condition, pageRequest);
+
+        assertThat(result.getSize()).isEqualTo(3);
+
+        // 반환된 데이터의 정렬 순서 확인
+        assertThat(result.getContent()).extracting("username")
+                .containsExactly("member4", "member3", "member2");
     }
 }
